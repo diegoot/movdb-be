da022e7a2fed0c0437a3772f4b7a6519ee256b20
Diego Toranzo <dstoranzo@gmail.com>
2019-06-13 18:49:02 -0300

    Connecting to database

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const app = express()
+const dbConnection = mongoose.connection
+const envConfig = dotenv.config()
 
-const port = 3014
+if (envConfig.error) {
+  console.error(envConfig.error)
+  process.exist(1)
+}
+
+dbConnection.on('error', error => {
+  console.error(error)
+  process.exit(1)
+})
+
+dbConnection.once('open', () => {
+  console.log(`Listening on port ${process.env.PORT}`)
+})
 
 app.use('/', (req, res, next) => {
   res.send('I am working')
 })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
 })
