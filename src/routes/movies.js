a1938071d93412682e86b26b67cfbf9bf553db4a
Diego Toranzo <dstoranzo@gmail.com>
2019-06-16 16:51:01 -0300

    Adding endpoint to retrieve a single movie

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -2,11 +2,22 @@ const express = require('express')
 const router = express.Router()
 const Movie = require('../models/Movie')
 
-// It returns all movies
+// It retrieves all movies
 router.get('/', async (req, res) => {
   try {
     const movies = await Movie.find()
     res.send(movies)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send()
+  }
+})
+
+// It retrieves a single movie
+router.get('/:id', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id)
+    res.send(movie)
   } catch (error) {
     console.error(error)
     res.status(500).send()
