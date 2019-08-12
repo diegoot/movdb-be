12c2a85389579e98553068787ba3cc1a7c6477f0
Diego Toranzo <dstoranzo@gmail.com>
2019-08-12 07:52:47 -0300

    Adding login endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 router.post('/signup', async (req, res, next) => {
@@ -12,6 +13,32 @@ router.post('/signup', async (req, res, next) => {
     })
     await user.save()
     res.status(201).send()
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (user === null) {
+      res.status(401).send('Authentication failed')
+      return
+    }
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+    if (!isPasswordValid) {
+      res.status(401).send('Authentication failed')
+      return
+    }
+    const token = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JWT_PRIVATE_KEY,
+      { expiresIn: '1h' }
+    )
+    res.send({ token })
   } catch (error) {
     next(error)
   }
